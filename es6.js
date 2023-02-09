87254fb32072c13f761527396b63ff74bff4814a
Chetan <itschetan.2095@gmail.com>
2023-02-09 16:56:39 +0530

    difference imong var, let and const

--- es6.js
+++ es6.js
@@ -1,8 +1,49 @@
 var x = 3;
-var y = "Utkarsh"
+var y = "Utkarsh";
 
 let a = 4;
 
-const p = "Albert"
+const p = "Albert";
 
 console.log(x, y, a, p);
+
+x = 5;
+y = "Riyaz";
+a = 10;
+
+// p = "Monika";
+
+console.log(x, y, a, p);
+
+// "var",  "let", "const"
+
+var x = 2;
+let y = 3;
+const z = 4;
+
+console.log(x, y, z);             //Global Scope:
+
+function abc() {
+                                  //Function Scope
+    var p = "a";
+    let q = "b";
+    const r = "c";
+
+    console.log(x, y, z);
+
+    if (y > x) {
+                                    //Block scope
+        var m = "m";
+        let n = "n";
+        const o = "o";
+        console.log(x, y, z, p, q, r, m, n, o);
+    }
+    console.log(m);      //"var" keyword is funcational scope variable
+    // console.log(n)    // "let" and "const" are keyword is block scope variable
+    // console.log(o)
+
+    // console.log(m, n, o)     //Variable created in a block can not be accesed outside it(excapt "var")
+}
+abc();
+
+// console.log(p, q, r, m, n, o)   //variable created in function and block scope can not to accesed outside of these scopes (except "var")
