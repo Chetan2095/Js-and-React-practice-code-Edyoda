94fcd2169c5099a9ebeaa63d0729b5ca75e85f6e
Chetan <itschetan.2095@gmail.com>
2023-02-09 18:50:12 +0530

    First Commit

---
+++ arrowFunction.js
@@ -0,0 +1,37 @@
+// console.log(myFunction)          //function hosting
+// function myFunction(){
+//     console.log("I am cool dude");
+// }
+
+// myFunction();      //function calling - function execution
+
+
+// console.log(myFun);     //"var" variable  hosting
+
+
+// var myFun = function(){
+//     console.log("Hi, this is another way of function creation");
+// }
+// myFun();      //function calling - function execution
+
+
+// Arrow Function:
+// var arrowFun = ()=>{
+//     console.log("I am an Arrow function");
+// }
+// arrowFun();     //function calling - function execution
+
+
+// var myFunction = (a,b)=>{
+//     return a + b;
+// }
+// const sum = myFunction(2,4);
+// console.log("The sum is", sum);
+
+// Arrow function is a self return function-
+// var add = (a,b)=>a+b;         //function declaration
+// const addition = add(4,6);    //function calling
+// console.log("The sum is", addition);
+
+
+
