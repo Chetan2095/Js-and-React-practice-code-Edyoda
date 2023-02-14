d8fed5989f6a80f90423b1a235443f01dfddefbf
Chetan <itschetan.2095@gmail.com>
2023-02-14 20:20:01 +0530

    used rest oprator in Object

--- rest.js
+++ rest.js
@@ -1,3 +1,18 @@
+//In Arrays:
 const arr = [1, 2, 3, 4, 5, 6];
 const[x, y, ...newArr] = arr;
 console.log(x, y, newArr);
+
+
+//In Objects:
+obj = {name : "Chetan",
+       address : {
+           city : "Jaipur",
+           pin : 12345
+           }, 
+       gender : "Male"
+       };
+       
+       const {...obj2} = obj      //spread oprator > spread all the values
+       const {name, ...newObj} = obj;  //rest operator > copy the rest values 
+console.log(name, newObj, obj2);
