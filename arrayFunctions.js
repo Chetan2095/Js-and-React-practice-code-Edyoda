7927dd10af2085c40cbd0818560d87e945989d34
Chetan <itschetan.2095@gmail.com>
2023-02-15 08:59:53 +0530

    map( ) function is added

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -7,7 +7,7 @@ const arr = [1, 2, 3, 4, 5]
 //variable with 'let' keyword are of block scope only
 //if we want to access 'i' outside the block, it should of with 'var' keyword because variable with 'var' keyword are of functional scope and can be access in whole function in which it is defined
 
-for(var i=0; i<arr.length; i++){
+for (var i = 0; i < arr.length; i++) {
     console.log(i);
 }
 console.log("value of 'i' after last iteration=", i);  //here i = 5
@@ -15,9 +15,9 @@ console.log("value of 'i' after last iteration=", i);  //here i = 5
 
 
 //Function:
-function abc(x){
+function abc(x) {
     // console.log(x);     //function declaration
-    console.log(x()); 
+    console.log(x());
 }
 // let s = 2;
 // let s = function(){
@@ -36,26 +36,54 @@ function abc(x){
 
 
 //arrow function- arow is a call back function > arrow function is nside another function
-abc(()=>{
-    console.log("Hello");       
+abc(() => {
+    console.log("Hello");
     return 99;
 })
 
 
+//Array functions:-
 
-// forEach() : 'for' loop + arrow function :-
+// forEach() : 'for' loop + arrow function :- it runs for each index and item. it does not return anything.
 // const arr = [1, 2, 3, 4, 5]
-arr.forEach((item, index, array)=>{
+arr.forEach((item, index, array) => {
     console.log(item, index, array);
 })
 
 // forEach() : 'for' loop + arrow function :-
 // const arr = [1, 2, 3, 4, 5]
-const output = arr.forEach((item, index, array)=>{
+const output = arr.forEach((item, index, array) => {
     console.log(item, index, array);
     return "xyz";          //undefined >> forEach() doesn't return anything
 })
 console.log(output);
+
+
+
+//map() - similar to forEach() but it returns new array. > 'for' loop + arrow function :- it runs for each index and item. it return a new array.
+// const arr = [1, 2, 3, 4, 5]
+// const output = arr.map((item, index, array )=>{
+//     console.log(item, index, array);
+//     // return item*2;
+//     return "xyz";
+// })
+// console.log(output);
+
+
+const output = arr.map((item, index, array) => {
+    console.log(item * 2, index * 3, array[index + 1]);
+    for (item == 3) {
+                                  //undefine for item = 3
+    }
+    else {
+    return item * 2;           //we can return anything in an new array
+}
+})
+console.log(output);
+
+
+
+
 
 
 
